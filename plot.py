b965bf62e22eac788193f390f0a389d4c8fc5425
Viktor Eisenstadt <vsa85@gmx.de>
2022-04-01 04:16:00 +0200

    reduce training steps

--- plot.py
+++ plot.py
@@ -12,7 +12,7 @@ for plt_type in ['auc', 'train_loss']:
         ylbl = 'Loss'
         clr = 'darkorange'
         y_from = 0.33
-        y_to = 0.96
+        y_to = 0.98
 
     with open(plt_type + '.csv', 'r') as csv:
         lines = csv.readlines()
@@ -23,7 +23,7 @@ for plt_type in ['auc', 'train_loss']:
             x_axis.append(int((data[0])))
             y_axis.append(float(str(data[1])[:4]))
         plt.ylim(y_from, y_to)
-        plt.xlim(0, 500)
+        plt.xlim(0, 150)
         plt.plot(x_axis,
                  y_axis,
                  linestyle='-',
@@ -31,14 +31,14 @@ for plt_type in ['auc', 'train_loss']:
                  color=clr,
                  label=ylbl + ' @ Step',
                  marker='s',
-                 markevery=50,
+                 markevery=15,
                  markersize=12,
                  markeredgecolor='black',
                  aa=True,
                  snap=True)
-        plt.annotate(str(y_axis[0]), xy=(x_axis[0], y_axis[0]), xytext=(10, 0), textcoords='offset points', size=12)
+        plt.annotate(str(y_axis[0]), xy=(x_axis[0], y_axis[0]), xytext=(10, -3), textcoords='offset points', size=12)
         plt.annotate(str(y_axis[len(y_axis) - 1]), xy=(x_axis[len(x_axis) - 1], y_axis[len(y_axis) - 1]),
-                     xytext=(5, 0), textcoords='offset points', size=12)
+                     xytext=(5, -3), textcoords='offset points', size=12)
 
     plt.xlabel(xlbl)
     plt.ylabel(ylbl)
