3c15bad1c05fe5c8a2ca7eae0fa54bab0666bea9
Viktor Eisenstadt <vsa85@gmx.de>
2023-01-07 22:31:22 +0100

    update lpapi run script

--- lpapi.sh
+++ lpapi.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-python3 server/lpapi/manage.py runserver 8001
+mkdir -p requests/paths \
+  && mkdir -p requests/connectivity/{1..5}_open \
+  && mkdir -p requests/connectivity/{1..5}_closed \
+  && mkdir -p requests/dataset_clean/{1..5}_open \
+  && mkdir -p requests/dataset_clean/{1..5}_closed \
+  && python3 server/lpapi/manage.py runserver 8001
